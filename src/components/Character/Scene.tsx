ec3f999251a42b6806ce6a873a2662e3562d0e1f
Nishanth Aradhya <aradhyanishanth84@gmail.com>
2026-03-11 15:23:18 +0530

    Fix typescript error in mobile landscape camera logic

--- src/components/Character/Scene.tsx
+++ src/components/Character/Scene.tsx
@@ -38,7 +38,7 @@ const Scene = () => {
       canvasDiv.current.appendChild(renderer.domElement);
 
       const camera = new THREE.PerspectiveCamera(14.5, aspect, 0.1, 1000);
-      const isMobile = window.innerWidth < 768;
+      const isMobile = window.innerWidth < 768 || (window.innerHeight < 500 && window.innerWidth < 1000);
       camera.position.z = 10;
       camera.position.set(0, isMobile ? 14.5 : 13.1, isMobile ? 32 : 24.7);
       camera.zoom = isMobile ? 0.85 : 1.1;
